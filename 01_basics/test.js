f4021ac2184f1581c95b13dd15618f3a13361c08
vg8085248 <vg8085248@gmail.com>
2026-02-13 16:40:43 +0000

    jstable

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("hello");
+// console.log("hello");
+
+const accountId = 12413324;
+let accountEmail = "vivek@google.com";
+var accountPassword = "1234123";
+// accountcity = prayagraj;
+
+console.table([accountEmail, accountId, accountPassword]);
